35f9ac65dcdaae3ef65634395a205d09f1a7578e
hugosilvag6 <hugosilvag6@hotmail.com>
2022-08-31 18:32:54 -0300

    Initial Commit

---
+++ iChat/iChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChatApp.swift
+//  iChat
+//
+//  Created by Hugo Silva on 31/08/22.
+//
+
+import SwiftUI
+
+@main
+struct iChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
